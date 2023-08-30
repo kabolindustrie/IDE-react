0ea082d7e43f936af0239a76acacda6f9c762ed2
Kabol Amine <amine.kabol@gmail.com>
2023-08-30 16:43:22 +0200

    mise en place de linterface

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 function App() {
   return (
-    <div>
+    <div className="bg-zinc-900 min-h-screen flex flex-col">
+      <div className="px-4 py-4 border-b flex items-center">
+        <h1 className="w-full text-slate-50 text-2xl">
+          <span className="text-base">the</span>SmartIDE
+        </h1>
+        <div className="w-full">
+          <button className="">Show preview</button>
+        </div>
+        <div className="ml-auto mr-2">
+          <div className="w-[6px] h-[6px] bg-slate-200 rounded-full my-1"></div>
+          <div className="w-[6px] h-[6px] bg-slate-200 rounded-full my-1"></div>
+          <div className="w-[6px] h-[6px] bg-slate-200 rounded-full"></div>
+        </div>
+      </div>
     </div>
   )
 }

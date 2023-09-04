154b9ff8230b8f50498545b3b69931829fc2166e
Kabol Amine <amine.kabol@gmail.com>
2023-09-04 13:02:34 +0200

    retour sur le projet, bug encore là aucune erreur en console, je pense que cest soit une erreur de synthaxe que je narrive pas à voir soit en rapport avec le useState

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -5,10 +5,12 @@ import ButtonTab from "./ButtonTab"
 
 export default function Tabs() {
   const tabs = useSelector(state => state.tabs)
+
   const [tabIndex, setTabIndex] = useState(tabs[0].id)
+  console.log(tabs);
   return (
     <div className="flex grow">
-      <div className="flex grow flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200">
+      <div className="grow flex flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200">
         {tabs.map(tab => (
           <ButtonTab
           key={tab.id}
--- src/features/tabs.js
+++ src/features/tabs.js
@@ -1,35 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit"
-import {html, js, css} from "../assets/index"
+import {html, css, js} from "../assets/index"
 
 const initialState = [
-  {
-    id: 1,
-    lang:"html",
-    imgURL: html,
-    buttonContent: "HTML",
-    code: `<div>
-    <h1>Editeur de code avec React</h1>
-    <p>Coder directement sur votre navigateur.</p> `
-  },
-  {
-    id:2,
-    lang:"css",
-    imgURL: css,
-    buttonContent: "CSS",
-    code: `body {
-      font-family: Roboto sans-serif;
-      padding: 25px;
-      color: #111;
-      background-color: #f1f1f1;
-    } `
-  },
-  {
-    id: 2,
-    lang:"javascript",
-    imgURL: js,
-    buttonContent: "JavaScript",
-    code: `console.log("Hello World"); `
-  }
+    {
+      id: 1,
+      lang: "html",
+      imgURL: html,
+      buttonContent: "HTML",
+      code: `<div>
+      <h1>Éditeur de code avec React</h1>
+      <p>Codez directement sur votre navigateur.</p>
+</div>
+      `
+    },
+    {
+      id: 2,
+      lang: "css",
+      imgURL: css,
+      buttonContent: "CSS",
+      code: `body {
+        font-family: Roboto, sans-serif;
+        padding: 25px;
+        color: #111;
+        background-color: #f1f1f1;
+      }`,
+    },
+    {
+      id: 3,
+      lang: "javascript",
+      imgURL: js,
+      buttonContent: "JavaScript",
+      code: `console.log("Hello World");`
+    }
 ]
 
 export const codeUpdater = createSlice({
@@ -44,3 +46,4 @@ export const codeUpdater = createSlice({
 
 export const {updateCode} = codeUpdater.actions
 export default codeUpdater.reducer
+
